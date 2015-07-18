bb5c4d9264590232990f0495ba09fc2761a74f25
Linus Unnebäck <linus@folkdatorn.se>
2015-07-18 19:43:16 +0200

    new build system

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fia med kort
+
+Fia med kort är en roligare variant av Fia med knuff. Denna variant innehåller
+speciella pjäser och 52 st spelkort.
+
+## Prerequisite
+
+```bash
+npm install
+```
+
+## Build PDFs
+
+```bash
+npm run build
+```
---
+++ build.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var path = require('path')
+var renderPDF = require('./lib/render-pdf')
+
+function render (name, cb) {
+  var outPath = path.join(__dirname, 'gen', name + '.pdf')
+
+  renderPDF(name, outPath, cb)
+}
+
+function done (err) {
+  if (err) throw err
+}
+
+if (process.argv[2]) {
+  render(process.argv[2], done)
+} else {
+  render('base', done)
+  render('events', done)
+}
